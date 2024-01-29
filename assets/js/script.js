489c06ed3d344d34f1e26042bf0b69171c7b458d
Ian Mucheru <imucheru.im@gmail.com>
2024-01-29 21:15:54 +0300

    Navbar Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,25 @@ const addEventOnElements = function (elements, evenType, callback) {
   }
 };
 
-// PRELOADING
+// PRELOAD
 const loadingElement = document.querySelector("[data-loading]");
 window.addEventListener("load", function () {
   loadingElement.classList.add("loaded");
   document.body.classList.remove("active");
 });
+
+// MOBILE NAV TOGGLE
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll("[data-nav-toggler"),
+  document.querySelectorAll("[data-nav-link]"),
+  document.querySelector("[data-navbar]"),
+  document.querySelector("[data-overlay]"),
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNav);
